5c0664e101f9297192bf649bb64dee25e62bdb90
Anurog1112 <popzxc899@gmail.com>
2026-02-03 18:32:26 +0700

    update

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,2 +1,9 @@
-read num
-echo "${num}"
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    i=1
+    while [ $i -le $# ] && [ $i -le 3 ]; do
+        echo "${!i}"
+        i=$((i + 1))
+    done
+fi
